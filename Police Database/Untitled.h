9f2824d44df8cee76f60bd3202741b3fb73b858b
Luis Herrera <luisherrera552@yahoo.com>
2024-09-22 11:44:11 -0500

    Began database

---
+++ Police Database/Untitled.h
@@ -0,0 +1,7 @@
+//
+//  Untitled.h
+//  Police Database
+//
+//  Created by Luis Herrera on 9/22/24.
+//
+
--- Police Database/main.cpp
+++ Police Database/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+#include <map>
+using namespace std;
+
+class Police{
+private:
+    string officerName;
+    int officerBadge;
+    string officerDistrict;
+    
+public:
+    Police (){}
+    Police (const string& name, int badge, const string& district):officerName(name), officerBadge(badge), officerDistrict (district){}
+    
+    void setPoliceName(const string& name){officerName = name;}
+    void setPoliceBadge(int badge){officerBadge = badge;}
+    void setPoliceDistrict(const string& district){ officerDistrict = district;}
+    
+    string getPoliceName() const {return officerName;}
+    int getPoliceBadge() {return officerBadge;}
+    string getPoliceDistrict() const {return officerDistrict;}
+    
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
     return 0;
 }
