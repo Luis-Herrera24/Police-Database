11b057525022cadcc9230b6cc0d65bfd349f6b0d
Luis Herrera <luisherrera552@yahoo.com>
2024-09-24 08:37:43 -0500

    Updated directory

--- Police Database/main.cpp
+++ Police Database/main.cpp
@@ -6,8 +6,16 @@
 //
 
 #include "DepartmentHeader.hpp"
+void enteringOfficersInfo();
 
 int main() {
+    
+    enteringOfficersInfo();
+
+    return 0;
+}
+void enteringOfficersInfo(){
+    
     PoliceDepartment Sheriff_Department;
     
     string fullname;
@@ -36,33 +44,32 @@ int main() {
         Sheriff_Department.exportInfoToFile("OfficerList.txt");
         badge++;
     }
-    
-    
-   
-    /*PoliceDepartment sheriffDepartment;
-    // string (name) , int (badge) , string (district), string (rank)
-    Police officer1("Luis Herrera", 5145, "East", "Deputy");
-    Police o2("Allison Mich", 4546, "west","Sr. Deputy");
-    // Test creating a blank officer
-    Police test;
-    
-    sheriffDepartment.addPoliceOfficer(officer1);
-    sheriffDepartment.addPoliceOfficer(o2);
-    sheriffDepartment.addPoliceOfficer(test);
-    
-    sheriffDepartment.updatePoliceName(4546, "Allison Micho");
-    sheriffDepartment.updatePoliceRank(5145, "Captain");
-    sheriffDepartment.updatePoliceDistrict(4546, "West");
-    sheriffDepartment.updatePoliceName(1234, "J. W Bush");
-    sheriffDepartment.updatePoliceRank(3567, "Master");
-    sheriffDepartment.updatePoliceDistrict(3246, "Central");
-    
-    sheriffDepartment.findPoliceOfficer(5145);
-     sheriffDepartment.findPoliceOfficer(4325);
-    
-    sheriffDepartment.displayAllPolice();
-    sheriffDepartment.removePoliceOfficer(5145);
-    sheriffDepartment.displayAllPolice();
-    */
-    return 0;
+    badge = badge;
 }
+
+
+/*PoliceDepartment sheriffDepartment;
+// string (name) , int (badge) , string (district), string (rank)
+Police officer1("Luis Herrera", 5145, "East", "Deputy");
+Police o2("Allison Mich", 4546, "west","Sr. Deputy");
+// Test creating a blank officer
+Police test;
+
+sheriffDepartment.addPoliceOfficer(officer1);
+sheriffDepartment.addPoliceOfficer(o2);
+sheriffDepartment.addPoliceOfficer(test);
+
+sheriffDepartment.updatePoliceName(4546, "Allison Micho");
+sheriffDepartment.updatePoliceRank(5145, "Captain");
+sheriffDepartment.updatePoliceDistrict(4546, "West");
+sheriffDepartment.updatePoliceName(1234, "J. W Bush");
+sheriffDepartment.updatePoliceRank(3567, "Master");
+sheriffDepartment.updatePoliceDistrict(3246, "Central");
+
+sheriffDepartment.findPoliceOfficer(5145);
+ sheriffDepartment.findPoliceOfficer(4325);
+
+sheriffDepartment.displayAllPolice();
+sheriffDepartment.removePoliceOfficer(5145);
+sheriffDepartment.displayAllPolice();
+*/
