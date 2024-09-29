67643a912d8f6757ac7f6e8ac52c781afdebc579
Luis Herrera <luisherrera552@yahoo.com>
2024-09-29 11:52:51 -0500

    working to get the program to work

--- Police Database/DepartmentHeader.hpp
+++ Police Database/DepartmentHeader.hpp
@@ -59,27 +59,56 @@ public:
 
 class PoliceDepartment{
 private:
+    
+    void createTableIfNotExists() {
+        sqlite3* dB;
+        char* errMsg = nullptr;
+
+        if (sqlite3_open("PoliceDatabase.db", &dB) != SQLITE_OK) {
+            cout << "Error! Unable to open database" << endl;
+            return;
+        }
+
+        const char* sql = "CREATE TABLE IF NOT EXISTS Officers ("
+                          "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                          "badge INTEGER, "
+                          "name TEXT, "
+                          "district TEXT, "
+                          "rank TEXT);";
+
+        //cout << "Executing SQL: " << sql << endl;  // Debugging output
+
+        if (sqlite3_exec(dB, sql, nullptr, nullptr, &errMsg) != SQLITE_OK) {
+            cout << "Error creating table: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        } else {
+            cout << "Table created successfully or already exists." << endl;
+        }
+
+        sqlite3_close(dB);
+    }
+    
     int readLastBadgeFromDatabase(){
         sqlite3* dB; // Declared pointed db type sqlite
         
-        int lastBadgeNum = 100; // Default badge number
+        int badge = 100; // Default badge number
         
         char* errMsg = nullptr; //Char pointer named errMsg used to capture any errors
         
         // attetmps to open databaase name "police_department.db" and stored in rc. if its sucessfull dB will point to the database
-        int rc = sqlite3_open("police_department.dB", &dB);
+        int rc = sqlite3_open("PoliceDatabase.db", &dB);
         
         // checks if "rc" is non-zero which would indicate error occured. Prints error message and returns badge 100;
         if(rc){
             cerr << "Error opening database: " << sqlite3_errmsg(dB) << endl;
-            return lastBadgeNum;
+            return badge;
         }
         // query string that contains a SQL statement which will get the highest badge # in database.
-        string query = "SELECT MAX(badge_number) FROM Officers;";
+        string query = "SELECT MAX(badge) FROM Officers;";
         
         /*executes a SQL query using pointer dB and query string above. It uses a lamda function [] passed as a callback
         void data - points to data (lastBadgeNum), int arg = # of collums (one in this case)
-        char arg - array of strings that will contain one column in current row (arg[0] will contain MAX(badge_number)
+        char arg - array of strings that will contain one column in current row (arg[0] will contain MAX(badge)
         char colNames - array of strings at contain the names of columns (not used here so its ingnored) */
         sqlite3_exec(dB,query.c_str(), [](void* data, int argc, char** argv, char** colNames){
             
@@ -91,7 +120,7 @@ private:
             }
             return 0;
             // this completes the lambda function and passes &lastBadge as the first argument (data), and &errMsg as the last argument to sqlite3_exec.
-        }, &lastBadgeNum, &errMsg);
+        }, &badge, &errMsg);
         // After the query execution, block checks if errMsg contains an error message. If it does, it prints the error message and frees the memory allocated for the error message using sqlite3_free
         if(errMsg){
             cerr << "Error opening database: " << errMsg << endl;
@@ -100,14 +129,14 @@ private:
         // closes database connection
         sqlite3_close(dB);
         
-        return lastBadgeNum;
+        return badge;
     }
     
     Police getOfficerFromDatbase(int badge){
         Police officer;
         // error handing if database does not open
         sqlite3* dB;
-        if(sqlite3_open("police_department.db", &dB) != SQLITE_OK){
+        if(sqlite3_open("PoliceDatabase.db", &dB) != SQLITE_OK){
             cout << "Error! Unable to open database" << endl;
             return officer;
         }
@@ -139,41 +168,45 @@ private:
         return officer;
     }
     
-    void insertOfficerIntoDatabase(const Police& officer){
+    void insertOfficerIntoDatabase(const Police& officer) {
         sqlite3* dB;
-        
-        if(sqlite3_open("police_department.db", &dB) != SQLITE_OK){
+
+        if (sqlite3_open("PoliceDatabase.db", &dB) != SQLITE_OK) {
             cout << "Error! Unable to open database" << endl;
             return;
         }
-        
+
         string sql = "INSERT INTO Officers (name, badge, district, rank) VALUES(?,?,?,?)";
+        cout << "Executing SQL: " << sql << endl; // Debugging output
         sqlite3_stmt* stmt;
-        
-        if(sqlite3_prepare_v2(dB,sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK){
+
+        if (sqlite3_prepare_v2(dB, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
             cout << "Error! Incorrect Statement" << endl;
             sqlite3_close(dB);
             return;
         }
+        
+        // Bind values
         sqlite3_bind_text(stmt, 1, officer.getPoliceName().c_str(), -1, SQLITE_STATIC);
         sqlite3_bind_int(stmt, 2, officer.getPoliceBadge());
-        sqlite3_bind_text(stmt, 3, officer.getPoliceDistrict().c_str(), -1,SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 3, officer.getPoliceDistrict().c_str(), -1, SQLITE_STATIC);
         sqlite3_bind_text(stmt, 4, officer.getPoliceRank().c_str(), -1, SQLITE_STATIC);
-        
-        if(sqlite3_step(stmt) != SQLITE_DONE){
+
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
             cout << "Error! Unable to insert Officer into Database" << endl;
-        }
-        else
+        } else {
             cout << "Officer was added to Database" << endl;
-        
+        }
+
         sqlite3_finalize(stmt);
         sqlite3_close(dB);
     }
+
     
     void displayOfficerFromDatabase(){
         sqlite3* dB;
         
-        if(sqlite3_open("policeDepartment.bd", &dB) != SQLITE_OK){
+        if(sqlite3_open("PoliceDatabase.db", &dB) != SQLITE_OK){
             cout << "Error! Unable to open database" << endl;
             return;
         }
@@ -186,76 +219,70 @@ private:
             sqlite3_close(dB);
             return;
         }
-        cout << "Officers in Daatabase:" << endl;
+        cout << "Officers in Database:" << endl;
         
         while(sqlite3_step(stmt)== SQLITE_ROW){
             cout << reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3)) << " "
             << reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)) << ", "
             << "Badge#: " << sqlite3_column_int(stmt, 1) << ", "
-            << "District " << reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4)) << endl;
+            << "District " << reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)) << endl;
         }
         sqlite3_finalize(stmt);
         sqlite3_close(dB);
         
     }
-    void updateOfficerFromDatabase(int badge,const string& name,const string& district, const string& rank){
+    void updateOfficerFromDatabase(int badge, const string& name, const string& district, const string& rank) {
         sqlite3* dB;
-        
-        if(sqlite3_open("policeDepartment.bd", &dB) != SQLITE_OK){
-            cout << "Error! Unable to open database" << endl;
+        char* errMsg = nullptr;
+        int rc = sqlite3_open("PoliceDatabase.db", &dB);
+
+        if (rc) {
+            cerr << "Error opening database: " << sqlite3_errmsg(dB) << endl;
             return;
         }
-        
-        string sql = "UPDATE Officers SET ";
-        bool first = true;
-        
-        if(!name.empty()){
-            sql+= "name = ?";
+
+        string query = "UPDATE Officers SET ";
+        bool first = true;  // To track if this is the first field being updated
+
+        // Dynamically add columns to update
+        if (!name.empty()) {
+            if (!first) query += ", "; // Add comma if it's not the first field
+            query += "Name = '" + name + "'";
             first = false;
         }
-        if(!district.empty()){
-            sql+= "district = ?";
+        if (!district.empty()) {
+            if (!first) query += ", ";
+            query += "District = '" + district + "'";
             first = false;
         }
-        if(!rank.empty()){
-            sql+= "rank = ?";
-            first = false;
+        if (!rank.empty()) {
+            if (!first) query += ", ";
+            query += "Rank = '" + rank + "'";
         }
-        sql+= "WHERE badge = ?;";
-        sqlite3_stmt* stmt;
-        
-        if(sqlite3_prepare_v2(dB, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK){
-            cout << "Error! Incorrect Statement" << endl;
-            sqlite3_close(dB);
-            return;
+
+        // Ensure that the query only runs if there is something to update
+        if (!first) {
+            query += " WHERE Badge = " + to_string(badge) + ";";
+
+            rc = sqlite3_exec(dB, query.c_str(), nullptr, nullptr, &errMsg);
+
+            if (rc != SQLITE_OK) {
+                cerr << "Error updating officer: " << errMsg << endl;
+                sqlite3_free(errMsg);
+            } else {
+                cout << "Officer's information was updated successfully!" << endl;
+            }
+        } else {
+            cout << "No updates made. All fields were left blank." << endl;
         }
-        
-        int index = 1;
-        if(!name.empty()){
-            sqlite3_bind_text(stmt, index++, name.c_str(), -1, SQLITE_STATIC);
-        }
-        if(!district.empty()){
-            sqlite3_bind_text(stmt, index++, district.c_str(), -1, SQLITE_STATIC);
-        }
-        if(!rank.empty()){
-            sqlite3_bind_text(stmt, index++, rank.c_str(), -1, SQLITE_STATIC);
-        }
-        
-        sqlite3_bind_int(stmt, index, badge);
-        
-        if(sqlite3_step(stmt) != SQLITE_DONE){
-            cout << "Error! Unable to update Officers infromation" << endl;
-        }
-        else
-            cout << "Officers infromation was updated!" << endl;
-        
-        sqlite3_finalize(stmt);
+
         sqlite3_close(dB);
     }
+
     void removeOfficerByBadge(int badge){
         sqlite3* dB;
         
-        if(sqlite3_open("policeDepartment.bd", &dB) != SQLITE_OK){
+        if(sqlite3_open("PoliceDatabase.db", &dB) != SQLITE_OK){
             cout << "Error! Unable to open database" << endl;
             return;
         }
@@ -279,6 +306,66 @@ private:
         sqlite3_finalize(stmt);
         sqlite3_close(dB);
     }
+    void checkTableStructure() {
+        sqlite3* dB;
+        char* errMsg = nullptr;
+        
+        // Open the database
+        int rc = sqlite3_open("PoliceDatabase.db", &dB);
+        if(rc) {
+            cerr << "Error opening database: " << sqlite3_errmsg(dB) << endl;
+            return;
+        }
+
+        // SQL query to check the table structure
+        const char* query = "PRAGMA table_info(Officers);";
+
+        // Execute the query
+        rc = sqlite3_exec(dB, query, [](void* NotUsed, int argc, char** argv, char** colNames) {
+            // Print each column's information
+            for(int i = 0; i < argc; i++) {
+                cout << colNames[i] << ": " << (argv[i] ? argv[i] : "NULL") << " | ";
+            }
+            cout << endl;
+            return 0;
+        }, nullptr, &errMsg);
+
+        if(rc != SQLITE_OK) {
+            cerr << "Error executing PRAGMA: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+
+        // Close the database
+        sqlite3_close(dB);
+    }
+    
+    void deleteTable() {
+        sqlite3* db;
+        char* errMsg = nullptr;
+
+        // Open the database
+        int rc = sqlite3_open("PoliceDatabase.db", &db);
+        if (rc) {
+            cerr << "Error opening database: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        // SQL statement to drop the Officers table
+        const char* sql = "DROP TABLE IF EXISTS Officers;";
+
+        // Execute the SQL command
+        rc = sqlite3_exec(db, sql, nullptr, nullptr, &errMsg);
+        if (rc != SQLITE_OK) {
+            cerr << "Error deleting table: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        } else {
+            cout << "Table deleted successfully." << endl;
+        }
+
+        // Close the database connection
+        sqlite3_close(db);
+    }
+
     
     
 public:
@@ -326,8 +413,18 @@ public:
     void updatePoliceRank(int badge, const string& rank){
         updateOfficerFromDatabase(badge,"","",rank);
     }
-    
-    
+    int getLastBadge() {
+            return readLastBadgeFromDatabase();
+        }
+    void createPoliceTable(){
+        createTableIfNotExists();
+    }
+    void checkTable(){
+        checkTableStructure();
+    }
+    void deleteT(){
+        deleteTable();
+    }
 };
 
 #endif
